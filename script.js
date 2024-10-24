c5c07080ecc78c71262463f68fc5892ab8f35afb
HeroOfDungeon <170803718+HeroOfDungeon@users.noreply.github.com>
2024-10-24 08:03:30 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,22 +1,30 @@
 let displayValue = '';
 let orderline1 = 1;
-let orderline2 = 2;
 let orderline3 = 3;
 var r = document.querySelector('.numbers');
 
 function appendToDisplay(value) {
     displayValue += value;
     document.getElementById('display').value = displayValue;
+    document.getElementById('icon_copy').style.visibility = 'visible';
+    document.getElementById('icon_check').style.visibility = 'hidden';
+    document.getElementById('copytext').style.cursor = 'pointer';
 }
 
 function clearDisplay() {
     displayValue = '';
     document.getElementById('display').value = '';
+    document.getElementById('icon_copy').style.visibility = 'visible';
+    document.getElementById('icon_check').style.visibility = 'hidden';
+    document.getElementById('copytext').style.cursor = 'pointer';
 }
 
 function deleteLast() {
     displayValue = displayValue.slice(0, -1);  // Lösche das letzte Zeichen
     document.getElementById('display').value = displayValue;
+    document.getElementById('icon_copy').style.visibility = 'visible';
+    document.getElementById('icon_check').style.visibility = 'hidden';
+    document.getElementById('copytext').style.cursor = 'pointer';
 }
 
 function calculate() {
@@ -31,10 +39,12 @@ function calculate() {
     } catch (error) {
         document.getElementById('display').value = 'Error';
     }
+    document.getElementById('icon_copy').style.visibility = 'visible';
+    document.getElementById('icon_check').style.visibility = 'hidden';
+    document.getElementById('copytext').style.cursor = 'pointer';
 }
 
 function toggleOrder() {
-    var rs = getComputedStyle(r);
     if (orderline1 == 1) {
         orderline1 = 3;
         orderline3 = 1;
@@ -42,10 +52,16 @@ function toggleOrder() {
         orderline1 = 1;
         orderline3 = 3;
     }
-    /*console.log(orderline1)
-    console.log(orderline3)*/
     r.style.setProperty('--orderline1', orderline1);
     r.style.setProperty('--orderline3', orderline3);
-    /*alert("The value of --orderline1 is: " + rs.getPropertyValue('--orderline1'));
-    alert("The value of --orderline3 is: " + rs.getPropertyValue('--orderline3'));*/
+}
+
+function copyValue() {
+    if (document.getElementById('icon_copy').style.visibility == 'visible') {
+        navigator.clipboard.writeText(displayValue)/*.then(() => {alert("Text erfolgreich kopiert");}).catch(() => {alert("Text kopieren nicht möglich");});*/
+        document.getElementById('icon_copy').style.visibility = 'hidden';
+        document.getElementById('icon_check').style.visibility = 'visible';
+        document.getElementById('copytext').style.cursor = 'default';
+        /*document.getElementById(icon_copy).value = 'check'*/
+    }
 }
